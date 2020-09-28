4ecc9fc9aea6f2f1edcd7b0d80a7b1efa84bc960
RBharti <44775866+RB786@users.noreply.github.com>
2020-09-29 00:04:31 +0200

    Add files via upload

---
+++ bin/compressed_fasta_genuslevel.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 23:36:23 2020
+
+@author: Richa Bharti
+"""
+
+from Bio import SeqIO
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='extracted uncompressed 16S rRNA fasta file')                                                  
+                                                                                                                                         
+parser.add_argument('fasta_file', type=str,                                                                                       
+                    help='extracted ncbi 16S rRNA fasta file for a motif')      
+                                                                                                                                         
+parser.add_argument('path_outputfile', type=str,
+                    help='path for all output file')
+
+args = parser.parse_args()                                                                                                               
+                                                                                                                                         
+input_file = args.fasta_file                                                                                               
+path_outputfile = args.path_outputfile
+#input_file = 'nusA_infB_genome_sequences.fasta'
+output_file = args.path_outputfile+'/'+input_file.split('/')[-1].split('.')[0] + '_reduced.fasta'
+
+w_handle = open(output_file,'w')
+
+
+records = list(SeqIO.parse(input_file, "fasta"))
+
+id_dict = dict()
+for i in range(0,len(records)):
+    wrd = records[i].description.split(' ')[1]
+    
+    if wrd in id_dict.keys():
+        continue
+    else:
+        id_dict[wrd] = 1
+        nid = records[i].name 
+        id_line = '>' + ' '.join(records[i].description.split(' ')[0:2]) + '\n'
+        seq = records[i]._seq._data + '\n'
+        w_handle.write(id_line)
+        seq_nl =  re.sub("(.{80})", "\\1\n", seq, 0, re.DOTALL)
+        w_handle.write(seq_nl)
+
+w_handle.close()
+        
+        
