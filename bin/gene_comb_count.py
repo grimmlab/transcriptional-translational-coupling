b2e409773a1c5f7bb5a70caadb5d80651824d780
RBharti <44775866+RB786@users.noreply.github.com>
2022-02-27 21:22:57 +0100

    Update gene_comb_count.py

--- bin/gene_comb_count.py
+++ bin/gene_comb_count.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  8 18:14:12 2020
+#!/usr/bin/python3
 
-@author: ge67mef
-"""
+__author__ = “Richa Bharti”
+__copyright__ = “Copyright 2019”
+__credits__ = [“Richa Bharti”]
+__license__ = “MIT”
+__version__ = “0.1.0”
+__maintainer__ = “Richa Bharti, Dominik Grimm”
+__email__ = “richabharti74@gmail.com”
+__status__ = “Dev”
 
 import textwrap
 import pandas as pd
